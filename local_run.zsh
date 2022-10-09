7ffb0b1c1faf7f4f7f419324d7808f722bc4e147
nachinius <ignacio.peixoto@gmail.com>
2022-10-10 01:59:56 +0200

    fix local run script

--- local_run.zsh
+++ local_run.zsh
@@ -3,12 +3,13 @@ set -euxo pipefail
 
 DB_PORT=${DB_PORT:-5432}
 
+OUTPUT=$(mktemp)
 docker run --rm --name jsonvalidatorpostgres \
   -e POSTGRES_PASSWORD=1 \
   -e POSTGRES_USER=app \
   -e POSTGRES_DB=db \
   -p "${DB_PORT}:5432" \
-  postgres
+  postgres >> $OUTPUT 2&>1 &
 echo "Docker spin up started."
 
 echo "Starting application"
