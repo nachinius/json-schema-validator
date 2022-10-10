d6f2f29cc5a514e7129b9608219bfe22e12c3fb2
nachinius <ignacio.peixoto@gmail.com>
2022-10-10 02:03:09 +0200

    download postgres image before running locally

--- local_run.zsh
+++ local_run.zsh
@@ -3,14 +3,16 @@ set -euxo pipefail
 
 DB_PORT=${DB_PORT:-5432}
 
+docker pull postgres
+
 OUTPUT=$(mktemp)
 docker run --rm --name jsonvalidatorpostgres \
   -e POSTGRES_PASSWORD=1 \
   -e POSTGRES_USER=app \
   -e POSTGRES_DB=db \
   -p "${DB_PORT}:5432" \
-  postgres >> $OUTPUT 2&>1 &
-echo "Docker spin up started."
+  postgres >> $OUTPUT 2>&1 &
+echo "Docker spin up started. "
 
 echo "Starting application"
 sbt run
